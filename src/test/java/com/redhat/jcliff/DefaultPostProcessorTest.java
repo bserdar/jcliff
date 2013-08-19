ce1b33cd4644fbcf66f563ac0bdc5c8e1573dec2
Burak Serdar <bserdar@redhat.com>
2013-08-19 12:11:15 -0600

    More tests

--- src/test/java/com/redhat/jcliff/DefaultPostProcessorTest.java
+++ src/test/java/com/redhat/jcliff/DefaultPostProcessorTest.java
@@ -24,6 +24,19 @@ public class DefaultPostProcessorTest {
     }
 
     @Test
+    public void simpleFail() throws Exception {
+        ModelNode[] ret=processor.process("{\n"+
+                                          "\"outcome\" => \"failed\",\n"+
+                                          "\"failure-description\" => \"JBAS014749: Operation handler failed: Service jboss.thread.executor.httpexecutor.thread-factory is already registered\",\n"+
+                                          "\"rolled-back\" => true\n"+
+                                          "}\n");
+
+        Assert.assertEquals(1,ret.length);
+        Assert.assertEquals(ModelType.OBJECT,ret[0].getType());
+        Assert.assertEquals("failed",ret[0].get("outcome").asString());
+    }
+
+    @Test
     public void failOut() throws Exception {
         ModelNode[] ret=processor.process("{\"JBAS014653: Composite operation failed and was rolled back. Steps that failed:\" => {\"Operation step-2\" => {\"JBAS014671: Failed services\" => {\"jboss.deployment.unit.\\\"assessment-service-ear-1.18.0.eap6.ear\\\".STRUCTURE\" => \"org.jboss.msc.service.StartException in service jboss.deployment.unit.\\\"assessment-service-ear-1.18.0.eap6.ear\\\".STRUCTURE: JBAS018733: Failed to process phase STRUCTURE of deployment \\\"assessment-service-ear-1.18.0.eap6.ear\\\"\n   Caused by: org.jboss.as.server.deployment.DeploymentUnitProcessingException: JBAS018746: Sub deployment assessment-service-util-1.18.0.eap6.jar in jboss-deployment-structure.xml was not found. Available sub deployments: assessment-service-ejb-1.18.0.eap6.jar, assessment-service-rest-1.18.0.eap6.war\"}}}}");
         Assert.assertEquals(1,ret.length);
