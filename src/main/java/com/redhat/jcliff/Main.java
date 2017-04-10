24b657abd31716071e265519704fba3bcdb66811
Marek Paterczyk <mpatercz@redhat.com>
2017-04-10 14:48:33 -0400

    Adding access log support for undertow

--- src/main/java/com/redhat/jcliff/Main.java
+++ src/main/java/com/redhat/jcliff/Main.java
@@ -29,7 +29,7 @@ import org.jboss.dmr.*;
  */
 public class Main {
 
-    private static final String VERSION="2.11.9";
+    private static final String VERSION="2.11.10";
 
     private static final HashSet<String> specialRules=new HashSet<String>();
 
