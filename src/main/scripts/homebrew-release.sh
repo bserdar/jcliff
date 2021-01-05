5eda67d18d17ff4bf7d5d03b7e14928adab83de1
Andrew Block <andy.block@gmail.com>
2021-01-05 08:58:45 -0600

    Updates based on feedback

--- src/main/scripts/homebrew-release.sh
+++ src/main/scripts/homebrew-release.sh
@@ -1,30 +1,34 @@
-set -e
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
 
-readonly JCLIFF_TAG="${{ steps.get_version.outputs.TAG_VERSION }}"
+readonly HOMEBREW_TAP_REPO_HOST=${HOMEBREW_TAP_REPO_HOST:-'github.com'}
 readonly HOMEBREW_TAP_REPO="${HOMEBREW_TAP_REPO:-ansible-middleware/homebrew-tap}"
 readonly REPO_NAME=$(basename $HOMEBREW_TAP_REPO)
+readonly GITHUB_RELEASES_URL=${GITHUB_RELEASES_URL:-'https://github.com/bserdar/jcliff/releases/download'}
+readonly DIST_TARBALL_URL=${DIST_TARBALL_URL:-"${GITHUB_RELEASES_URL}/${JCLIFF_TAG}/jcliff-${JCLIFF_TAG:1}-dist.tar.gz"}
+readonly FORMULA_PATH="${FORMULA_PATH:-'Formula/jcliff.rb'}"
 
-readonly SHA256=$(sha256sum target/jcliff-${JCLIFF_TAG:1}-dist.tar.gz | cut -f1 -d ' ')
-readonly URL="https://github.com/bserdar/jcliff/releases/download/${JCLIFF_TAG}/jcliff-${JCLIFF_TAG:1}-dist.tar.gz"
+readonly SHA256=$(sha256sum "target/jcliff-${JCLIFF_TAG:1}-dist.tar.gz" | cut -f1 -d ' ')
 
 rm -rf "${REPO_NAME}"
 
-git clone https://github.com/${HOMEBREW_TAP_REPO}.git
+git clone "https://${HOMEBREW_TAP_REPO_HOST}/${HOMEBREW_TAP_REPO}.git"
 
-sed -i -e 's|^  url.*|  url "'"$URL"'"|g' ${REPO_NAME}/Formula/jcliff.rb
-sed -i -e 's|^  sha256.*|  sha256 "'"$SHA256"'"|g' ${REPO_NAME}/Formula/jcliff.rb
+sed -i -e 's|^  url.*|  url "'"$DIST_TARBALL_URL"'"|g' "${REPO_NAME}/${FORMULA_PATH}"
+sed -i -e 's|^  sha256.*|  sha256 "'"$SHA256"'"|g' "${REPO_NAME}/${FORMULA_PATH}"
 
 cd $(basename "$HOMEBREW_TAP_REPO")
 
-git config user.name "${BREW_GITHUB_USER:-Andrew Block}"
-git config user.email "${BREW_GITHUB_EMAIL:-andy.block@gmail.com}"
+git config user.name "${BREW_GITHUB_USER:-'Andrew Block'}"
+git config user.email "${BREW_GITHUB_EMAIL:-'andy.block@gmail.com'}"
 
-git add Formula/jcliff.rb
+git add "${FORMULA_PATH}"
 git commit -m "jcliff ${JCLIFF_TAG} release"
 git tag -a "jcliff-${JCLIFF_TAG}" -m "jcliff ${JCLIFF_TAG}"
 
-if [[ ! -z $BREW_GITHUB_USERNAME && ! -z $BREW_GITHUB_TOKEN ]]; then
-git push https://${BREW_GITHUB_USERNAME}:${BREW_GITHUB_TOKEN}@github.com/${HOMEBREW_TAP_REPO}.git --follow-tags
+if [[ ! -z "${BREW_GITHUB_USERNAME}" && ! -z "${BREW_GITHUB_TOKEN}" ]]; then
+git push "https://${BREW_GITHUB_USERNAME}:${BREW_GITHUB_TOKEN}@${HOMEBREW_TAP_REPO_HOST}/${HOMEBREW_TAP_REPO}.git" --follow-tags
 else
 echo "BREW_GITHUB_USERNAME and BREW_GITHUB_TOKEN variables not set. Skipping push..."
 fi
